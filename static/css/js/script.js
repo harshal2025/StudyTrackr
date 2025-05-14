f81b41ec20c3c508e9da44c786735b5abe46ca2a
harshal2025 <harshalporwal1711@gmail.com>
2025-05-14 18:09:44 +0530

    Update script.js

--- static/css/js/script.js
+++ static/css/js/script.js
@@ -1,1 +1,13 @@
+window.onload = function () {
+    console.log("StudyTrackr loaded 🚀");
+
+    if (window.location.pathname === "/subjects") {
+        const btn = document.getElementById("helloBtn");
+        if (btn) {
+            btn.addEventListener("click", function () {
+                alert("Keep grinding, legend! 📚💪");
+            });
+        }
+    }
+};
 
